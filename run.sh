eec52f47f13d90040b5b15286ae0e884438319e5
Nil <xetaxe@gmail.com>
2023-11-17 19:53:50 +0100

    Add run scripts to handle concurrent frontend and backend

---
+++ run.sh
@@ -0,0 +1,28 @@
+case "$1" in
+  "front")
+    (cd frontend; npm run dev)
+    ;;
+
+  "back")
+    (cd backend; python run.py)
+    ;;
+
+  "dev")
+    (cd frontend; npm run dev) &
+    (cd backend; python run.py)
+    ;;
+
+  "build")
+    (cd frontend; npm run build)
+    ;;
+
+  "start")
+    (cd frontend; npm run preview) &
+    (cd backend; python run.py)
+    ;;
+
+  *)
+    echo "The argument provided is not valid"
+    exit 1
+    ;;
+esac
