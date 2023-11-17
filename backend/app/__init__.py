50957e992424346bb33a21029aca5a83dcd9608a
Nil <xetaxe@gmail.com>
2023-11-17 20:37:14 +0100

    Reframe backend structure (no DDD)

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from .routes.v1 import api_bp_v1
+from .routes.v1 import api_v1
 
 def create_app():
     app = Flask(__name__)
 
-    app.register_blueprint(api_bp_v1)
+    app.register_blueprint(api_v1)
 
     return app
--- backend/app/routes/v1/tasks.py
+++ backend/app/routes/v1/tasks.py
@@ -1,7 +1,26 @@
-# Example: tasks.py
-from . import api_bp_v1
+from flask import request
+from . import api_v1
 
-@api_bp_v1.route("/tasks")
-def get_tasks():
-    # Logic to fetch tasks
-    return 'Hello'
+# Route for handling GET and POST requests
+@api_v1.route('/tasks', methods=['GET', 'POST'])
+def tasks():
+    if request.method == 'GET':
+        # Handle GET request
+        return 'GET Request: Retrieve tasks'
+
+    elif request.method == 'POST':
+        # Handle POST request
+        new_task = request.json
+        return f'POST Request: Create task - {new_task}'
+
+# Route for handling PUT and DELETE requests for a specific task
+@api_v1.route('/tasks/<int:task_id>', methods=['PUT', 'DELETE'])
+def specific_task(task_id):
+    if request.method == 'PUT':
+        # Handle PUT request
+        updated_task = request.json
+        return f'PUT Request: Update task {task_id} - {updated_task}'
+
+    elif request.method == 'DELETE':
+        # Handle DELETE request
+        return f'DELETE Request: Delete task {task_id}'
